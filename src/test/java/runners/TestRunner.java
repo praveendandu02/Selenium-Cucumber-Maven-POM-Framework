2ec0ebfc19e55e136b35a646a128e9dee7a5bb50
pravinkumar <pravinkumar@192.168.1.64>
2019-03-22 11:24:51 +0000

    Developed Cucumber Reports

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,8 +7,13 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/functionalTests",
-glue={"stepDefinitions"})
+@CucumberOptions
+(features="src/test/resources/functionalTests",
+glue={"stepDefinitions"},
+plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+		 "junit:target/cucumber-reports/Cucumber.xml",
+		 "html:target/cucumber-reports"},
+monochrome = true)
 
 public class TestRunner{
 
